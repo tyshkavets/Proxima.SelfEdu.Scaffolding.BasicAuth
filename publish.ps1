4523980ead17e7646ce9eb3f2917ad931ba6cf42
Ty <tyshkovets@outlook.com>
2023-01-08 23:40:43 +0000

    Adding a powershell script for packing and publishing

---
+++ publish.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [string]$pathToSecrets = "../secrets/",
+    [string]$apiKeyFile = "SelfEduNugetPushKey",
+    [string]$nugetSource = "nuget.org",
+    [switch]$signKey,
+    [switch]$clear
+)
+
+if($clear)
+{
+    Remove-Item .\PackageOutput\ -Recurse
+}
+
+dotnet pack -c Release --output .\PackageOutput
+$apiKey = Get-Content "$pathToSecrets$apiKeyFile" -Raw
+if ($signKey)
+{
+    nuget push -Source "$nugetSource" -ApiKey "$apiKey" .\PackageOutput\
+}
+else
+{
+    nuget push -Source "$nugetSource" .\PackageOutput\
+}
